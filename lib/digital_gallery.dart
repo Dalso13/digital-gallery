31e5c3e6b03e30cd12d6250f808d7647ab064fe7
Dalso <flskwhdhk@gmail.com>
2023-12-21 15:13:15 +0900

    이미지 피커를 활용한 갤러리 완성 및 슬라이드 효과 구현

--- lib/digital_gallery.dart
+++ lib/digital_gallery.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DigtalGalleryApp extends StatefulWidget {
   const DigtalGalleryApp({super.key});
@@ -8,11 +12,69 @@ class DigtalGalleryApp extends StatefulWidget {
 }
 
 class _DigtalGalleryAppState extends State<DigtalGalleryApp> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+
+  int currentPage = 0;
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    // List 로 이미지 가져오기
+    images = await _picker.pickMultiImage();
+
+    if (images != null) {
+      Timer.periodic(Duration(seconds: 5), (timer) {
+        currentPage++;
+
+        if (currentPage > images!.length - 1) {
+          currentPage = 0;
+        }
+
+        pageController.animateToPage(
+            currentPage, duration: Duration(milliseconds: 350),
+            curve: Curves.easeIn);
+      });
+    }
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('전자액자'),
+      ),
+      body: images == null
+          ? Center(
+        child: Text('no data'),
+      )
+          : PageView(
+        controller: pageController,
+        children: images!.map((e) {
+          return FutureBuilder<Uint8List>(
+              future: e.readAsBytes(),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+
+                if (data == null ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Image.memory(
+                  data,
+                  width: double.infinity,
+                );
+              }
+          );
+        }).toList(),
       ),
     );
   }
